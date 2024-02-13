74915c7b0f1d4759f06f5c9f783d12cace377f30
Shub3am <shubhamvishwakarma0604@gmail.com>
2024-02-13 12:34:48 +0530

    Updating Personal Info

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,5 @@
 export const LINKS = {
   github: "https://github.com/shub3am",
   linkedin: "https://www.linkedin.com/in/shubhamvs/",
-  medium: "https://www.fiverr.com/users/shubhamvs",
   discord: "https://discordapp.com/users/683219705263030362",
 };
