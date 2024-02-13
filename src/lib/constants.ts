65b5b002171da5ae8d284a6c9d92b5c5048c46e6
Shub3am <shubhamvishwakarma0604@gmail.com>
2024-02-13 12:41:13 +0530

    Social Link Update

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,4 +2,5 @@ export const LINKS = {
   github: "https://github.com/shub3am",
   linkedin: "https://www.linkedin.com/in/shubhamvs/",
   discord: "https://discordapp.com/users/683219705263030362",
+  contra: "https://contra.com/shubham_x66yxv9d"
 };
